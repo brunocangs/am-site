c2f8a27126b925a7a96472e665380134ea83b964
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-10-04 09:52:16 -0300

    Update

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,11 @@
 import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
-import ProjectPagePreview from "./preview-templates/ProjectPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate("project", ProjectPagePreview);
+// CMS.registerPreviewTemplate("project", ProjectPagePreview);
 // CMS.registerPreviewTemplate('index', IndexPagePreview)
 // CMS.registerPreviewTemplate('about', AboutPagePreview)
 // CMS.registerPreviewTemplate('products', ProductPagePreview)
