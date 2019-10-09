471a902302c8c9e42e041333916368ad12cffb63
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-10-09 09:56:35 -0300

    Updated config.yml

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,1 +1,16 @@
-import "netlify-cms-app";
+import CMS from "netlify-cms-app";
+
+const ColorPickerControl = CMS.createClass({
+  handleChange: function(e) {
+    this.props.onChange(e.target.value);
+  },
+  render: function() {
+    return CMS.h("input", {
+      type: "color",
+      value: this.props.value,
+      onChange: this.handleChange
+    });
+  }
+});
+
+CMS.registerWidget("color", ColorPickerControl);
