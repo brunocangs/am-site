900b89436a82e5a12abaed9e66850f1799d31f30
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-10-16 11:59:39 -0300

    Updating schema for authors

---
+++ src/components/BlogComponents.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { BaseContainer } from "./ProjectComponents";
+
+export const AllBlogContainer = styled(BaseContainer)``;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
