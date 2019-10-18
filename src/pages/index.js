059191083e1325fdf8fb2af5657be96a9a6774c6
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-10-18 09:43:54 -0300

    Removed redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 
 export default () => {
-  useEffect(() => {
-    if (typeof navigator !== "undefined") {
-      if (navigator.language.toLowerCase().startsWith("pt"))
-        window.location = "/pt";
-      else window.location = "/en";
-    } else {
-      window.location = "/en";
-    }
-  });
+  // useEffect(() => {
+  //   if (typeof navigator !== "undefined") {
+  //     if (navigator.language.toLowerCase().startsWith("pt"))
+  //       window.location = "/pt";
+  //     else window.location = "/en";
+  //   } else {
+  //     window.location = "/en";
+  //   }
+  // });
   return null;
 };
