ed88eb21294ead75131cf83b96006f93a4ea0adc
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-11-06 09:15:06 -0300

    Fix para login em root

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
-
+import { Banner } from "../components/Header";
+import React from "react";
+import { Link } from "gatsby";
 export default () => {
   useEffect(() => {
-    if (typeof navigator !== "undefined") {
-      if (navigator.language.toLowerCase().startsWith("pt"))
-        window.location = "/pt";
-      else window.location = "/en";
-    } else {
-      window.location = "/en";
-    }
+    // if (typeof navigator !== "undefined") {
+    //   if (navigator.language.toLowerCase().startsWith("pt"))
+    //     window.location = "/pt";
+    //   else window.location = "/en";
+    // } else {
+    //   window.location = "/en";
+    // }
   });
-  return null;
+  return (
+    <div style={{ width: "100%" }}>
+      <Banner title="Oops" />
+      <p>
+        <Link to="/pt">Página em portuges</Link>
+      </p>
+      <p>
+        <Link to="/en">English page</Link>
+      </p>
+    </div>
+  );
 };
