bb44361fb18838cd475e143e75e3bc7732655290
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-11-18 12:36:55 -0300

    Enabling CMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import { Banner } from "../components/Header";
 import React from "react";
 import { Link } from "gatsby";
 export default () => {
-  useEffect(() => {
-    if (typeof navigator !== "undefined") {
-      if (navigator.language.toLowerCase().startsWith("pt"))
-        window.location = "/pt";
-      else window.location = "/en";
-    } else {
-      window.location = "/pt";
-    }
-  });
+  // useEffect(() => {
+  //   if (typeof navigator !== "undefined") {
+  //     if (navigator.language.toLowerCase().startsWith("pt"))
+  //       window.location = "/pt";
+  //     else window.location = "/en";
+  //   } else {
+  //     window.location = "/pt";
+  //   }
+  // });
   return null;
 };
